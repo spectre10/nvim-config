ddbfe828897b3e25eb868d82b468b5ef53b38a30
spectre10 <shyamthakkar001@gmail.com>
2023-12-11 17:17:20 +0530

    update config
    Signed-off-by: spectre10 <shyamthakkar001@gmail.com>

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
 vim.api.nvim_create_autocmd({ "VimEnter", "BufNew" }, {
   pattern = "*.go",
-  command = "map <C-f> <cmd>GoImport<CR>",
+  command = "nmap <C-f> :GoImport<CR>",
+  group = format_sync_grp,
+})
+vim.api.nvim_create_autocmd({ "VimEnter", "BufNew" }, {
+  pattern = "*.go",
+  command = "imap <C-f> <Esc>:GoImport<CR>a",
   group = format_sync_grp,
 })
 
@@ -10,12 +15,12 @@ vim.api.nvim_create_autocmd({ "VimEnter", "BufNew" }, {
   command = "set list listchars=tab:»\\ ,trail:·,eol:↲",
 })
 
-local compile_cpp = vim.api.nvim_create_augroup("run_cpp", {})
+local run_cpp = vim.api.nvim_create_augroup("run_cpp", {})
 vim.api.nvim_create_autocmd({ "VimEnter", "BufNew" }, {
   pattern = "*.cpp",
   command =
   "map <F9> :lua vim.lsp.buf.format()<CR>:w<CR>:T cd %:p:h && g++ %:p -std=c++17 -fsanitize=undefined -fsanitize=address && ./a.out < input.txt && exit<CR>i",
-  group = compile_cpp,
+  group = run_cpp,
 })
 
 return {
