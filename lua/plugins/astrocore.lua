8731d27c7148c59e6da253317d5769e46293cddd
Ghanshyam Thakkar <shyamthakkar001@gmail.com>
2024-12-09 17:42:31 +0530

    update

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -7,83 +7,97 @@
 
 ---@type lazyspec
 return {
-  "astronvim/astrocore",
-  ---@type astrocoreopts
-  opts = {
-    autocmds = {
-      newline_char = {
-        {
-          event = {"VimEnter", "BufNew"},
-          pattern = "*",
-          command = "set list listchars=tab:»\\ ,trail:·,eol:↲"
-        }
-      }
-    },
-    -- configure core features of astronvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight urls at start
-      notifications = true, -- enable notifications at start
-    },
-    -- diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- note: `mapleader` and `maplocalleader` must be set in the astronvim opts or before `lazy.setup`
-        -- this can be found in the `lua/lazy_setup.lua` file
-      },
-    },
-    -- mappings can be configured through astrocore as well.
-    -- note: keycodes follow the casing in the vimdocs. for example, `<leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      n = {
-        -- second key is the lefthand side of the map
+	"astronvim/astrocore",
+	---@type astrocoreopts
+	opts = {
+		autocmds = {
+			newline_char = {
+				{
+					event = { "VimEnter", "BufNew" },
+					pattern = "*",
+					command = "set list listchars=tab:»\\ ,trail:·,eol:↲",
+				},
+			},
+		},
+		-- configure core features of astronvim
+		features = {
+			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+			autopairs = true, -- enable autopairs at start
+			cmp = true, -- enable completion at start
+			diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+			highlighturl = true, -- highlight urls at start
+			notifications = true, -- enable notifications at start
+		},
+		-- diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+		diagnostics = {
+			virtual_text = true,
+			underline = true,
+		},
+		-- vim options can be configured here
+		options = {
+			opt = { -- vim.opt.<key>
+				relativenumber = true, -- sets vim.opt.relativenumber
+				number = true, -- sets vim.opt.number
+				spell = false, -- sets vim.opt.spell
+				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+				wrap = false, -- sets vim.opt.wrapreturn {
+				showtabline = 4,
+				shiftwidth = 4,
+				tabstop = 4,
+				colorcolumn = "80",
+			},
+			g = { -- vim.g.<key>
+				-- configure global vim variables (vim.g)
+				-- note: `mapleader` and `maplocalleader` must be set in the astronvim opts or before `lazy.setup`
+				-- this can be found in the `lua/lazy_setup.lua` file
+			},
+		},
+		-- mappings can be configured through astrocore as well.
+		-- note: keycodes follow the casing in the vimdocs. for example, `<leader>` must be capitalized
+		mappings = {
+			-- first key is the mode
+			n = {
+				-- second key is the lefthand side of the map
 
-        -- navigate buffer tabs
-        ["L"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "next buffer" },
-        ["H"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "previous buffer" },
+				-- navigate buffer tabs
+				["L"] = {
+					function()
+						require("astrocore.buffer").nav(vim.v.count1)
+					end,
+					desc = "next buffer",
+				},
+				["H"] = {
+					function()
+						require("astrocore.buffer").nav(-vim.v.count1)
+					end,
+					desc = "previous buffer",
+				},
 
-        -- mappings seen under group name "buffer"
-        ["<leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "close buffer from tabline",
-        },
+				-- mappings seen under group name "buffer"
+				["<leader>bd"] = {
+					function()
+						require("astroui.status.heirline").buffer_picker(function(bufnr)
+							require("astrocore.buffer").close(bufnr)
+						end)
+					end,
+					desc = "close buffer from tabline",
+				},
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<leader>b"] = { desc = "buffers" },
+				-- tables with just a `desc` key will be registered with which-key if it's installed
+				-- this is useful for naming menus
+				-- ["<leader>b"] = { desc = "buffers" },
 
-        -- setting a mapping to false will disable it
-        -- ["<c-s>"] = false,
-      },
-      v = {
-        ["p"] = { "P" },
-      },
-      -- i = {
-      --   -- map copilot's tab completion to <c-l>
-      --   ["<c-l>"] = { 'copilot#accept("\\<cr>")' },
-      --   
-      -- }
-    },
-  },
+				-- setting a mapping to false will disable it
+				-- ["<c-s>"] = false,
+			},
+			v = {
+				["p"] = { "P" },
+			},
+			-- i = {
+			--   -- map copilot's tab completion to <c-l>
+			--   ["<c-l>"] = { 'copilot#accept("\\<cr>")' },
+			--
+			-- }
+		},
+	},
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,120 +6,136 @@
 ---@type LazySpec
 return {
 
-  -- == Examples of Adding Plugins ==
+	-- == Examples of Adding Plugins ==
 
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+	"andweeb/presence.nvim",
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").setup()
+		end,
+	},
 
-  -- == Examples of Overriding Plugins ==
+	-- == Examples of Overriding Plugins ==
 
-  -- customize alpha options
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        -- " █████  ███████ ████████ ██████   ██████",
-        -- "██   ██ ██         ██    ██   ██ ██    ██",
-        -- "███████ ███████    ██    ██████  ██    ██",
-        -- "██   ██      ██    ██    ██   ██ ██    ██",
-        -- "██   ██ ███████    ██    ██   ██  ██████",
-        -- " ",
-        -- "    ███    ██ ██    ██ ██ ███    ███",
-        -- "    ████   ██ ██    ██ ██ ████  ████",
-        -- "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        -- "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        -- "    ██   ████   ████   ██ ██      ██",
-      }
-      return opts
-    end,
-  },
-  { "max397574/better-escape.nvim", enabled = true },
+	-- customize alpha options
+	{
+		"goolord/alpha-nvim",
+		opts = function(_, opts)
+			-- customize the dashboard header
+			opts.section.header.val = {
+				-- " █████  ███████ ████████ ██████   ██████",
+				-- "██   ██ ██         ██    ██   ██ ██    ██",
+				-- "███████ ███████    ██    ██████  ██    ██",
+				-- "██   ██      ██    ██    ██   ██ ██    ██",
+				-- "██   ██ ███████    ██    ██   ██  ██████",
+				-- " ",
+				-- "    ███    ██ ██    ██ ██ ███    ███",
+				-- "    ████   ██ ██    ██ ██ ████  ████",
+				-- "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+				-- "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+				-- "    ██   ████   ████   ██ ██      ██",
+			}
+			return opts
+		end,
+	},
+	{ "max397574/better-escape.nvim", enabled = true },
 
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      transparent_background = true,
-    },
-  },
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-    end,
-  },
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.completion.copilot-lua" },
-  {
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.editing-support.vim-visual-multi" },
-  {
-    "mg979/vim-visual-multi",
-    config = function()
-      vim.g.VM_default_mappings = 0
-      vim.g.VM_leader = "<Leader>m"
-      vim.g.VM_maps = {
-        select = "v",
-        select_line = "V",
-        -- select_word = "<C-n>",
-        add_cursor = "<C-n>",
-        remove_cursor = "<C-x>",
-        -- jump_to_next = "<C-n>",
-        -- jump_to_prev = "<C-p>",
-      }
-    end,
-  },
+	-- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+	{
+		"L3MON4D3/LuaSnip",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
+			-- add more custom luasnip configuration such as filetype extend or custom snippets
+			local luasnip = require("luasnip")
+			luasnip.filetype_extend("javascript", { "javascriptreact" })
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
+			-- add more custom autopairs configuration such as custom rules
+			local npairs = require("nvim-autopairs")
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+			npairs.add_rules(
+				{
+					Rule("$", "$", { "tex", "latex" })
+						-- don't add a pair if the next character is %
+						:with_pair(cond.not_after_regex("%%"))
+						-- don't add a pair if  the previous character is xxx
+						:with_pair(
+							cond.not_before_regex("xxx", 3)
+						)
+						-- don't move right when repeat character
+						:with_move(cond.none())
+						-- don't delete if the next character is xx
+						:with_del(cond.not_after_regex("xx"))
+						-- disable adding a newline when you press <cr>
+						:with_cr(cond.none()),
+				},
+				-- disable for .vim files, but it work for another filetypes
+				Rule("a", "a", "-vim")
+			)
+		end,
+	},
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.completion.copilot-lua" },
+	{
+		"copilot.lua",
+		opts = {
+			suggestion = {
+				keymap = {
+					accept = "<C-l>",
+					accept_word = false,
+					accept_line = false,
+					next = "<C-.>",
+					prev = "<C-,>",
+					dismiss = "<C/>",
+				},
+			},
+		},
+	},
+	-- { import = "astrocommunity.completion.copilot-lua-cmp" },
+	{ import = "astrocommunity.editing-support.vim-visual-multi" },
+	{
+		"mg979/vim-visual-multi",
+		config = function()
+			vim.g.VM_default_mappings = 0
+			vim.g.VM_leader = "<Leader>m"
+			vim.g.VM_maps = {
+				select = "v",
+				select_line = "V",
+				-- select_word = "<C-n>",
+				add_cursor = "<C-n>",
+				remove_cursor = "<C-x>",
+				-- jump_to_next = "<C-n>",
+				-- jump_to_prev = "<C-p>",
+			}
+		end,
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			background_colour = "#000000",
+			-- background_opacity = 0.8,
+			-- icons = {
+			--   ERROR = "",
+			--   WARN = "",
+			--   INFO = "",
+			--   DEBUG = "",
+			--   TRACE = "✎",
+			-- },
+		},
+	},
 }
